0d526c508fcefdcb0a4e201df5aebc8f419d6f93
islamelhady <i.elhady44@gmail.com>
2019-10-06 01:02:06 +0200

    add retrofit,recyclerview

--- app/src/main/java/com/elhady/newsapp/MainActivity.java
+++ app/src/main/java/com/elhady/newsapp/MainActivity.java
@@ -1,14 +1,124 @@
 package com.elhady.newsapp;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.app.AlertDialog;
+import com.elhady.newsapp.Interface.NewsService;
+import com.elhady.newsapp.adapter.ListSourceAdapter;
+import com.elhady.newsapp.common.Common;
+import com.elhady.newsapp.models.WebSite;
+import com.google.gson.Gson;
+
+import dmax.dialog.SpotsDialog;
+import io.paperdb.Paper;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView listWebsite;
+    RecyclerView.LayoutManager layoutManager;
+    NewsService mService;
+    ListSourceAdapter adapter;
+    AlertDialog dialog;
+    SwipeRefreshLayout swipeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Init cache
+        Paper.init(this);
+
+        //Init Service
+        mService = Common.getNewsService();
+
+        //Init View
+        swipeLayout = (SwipeRefreshLayout)findViewById(R.id.swipeRefresh);
+        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadWebsiteSource(true);
+            }
+        });
+
+        listWebsite = (RecyclerView)findViewById(R.id.list_source);
+        listWebsite.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        listWebsite.setLayoutManager(layoutManager);
+
+        dialog = new SpotsDialog(this);
+
+        loadWebsiteSource(false);
+    }
+
+    private void loadWebsiteSource(boolean isRefreshed) {
+        if(!isRefreshed)
+        {
+
+            String cache = Paper.book().read("cache");
+            if(cache != null && !cache.isEmpty() && !cache.equals("null")) // If have cache
+            {
+                WebSite website = new Gson().fromJson(cache,WebSite.class); // Convert cache from Json to Object
+                adapter = new ListSourceAdapter(getBaseContext(),website);
+                adapter.notifyDataSetChanged();
+                listWebsite.setAdapter(adapter);
+            }
+            else // If not have cache
+            {
+                dialog.show();
+                //Fetch new data
+                mService.getSources().enqueue(new Callback<WebSite>() {
+                    @Override
+                    public void onResponse(Call<WebSite> call, Response<WebSite> response) {
+                        adapter  = new ListSourceAdapter(getBaseContext(),response.body());
+                        adapter.notifyDataSetChanged();
+                        listWebsite.setAdapter(adapter);
+
+                        //Save to cache
+                        Paper.book().write("cache",new Gson().toJson(response.body()));
+
+                        dialog.dismiss();
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<WebSite> call, Throwable t) {
+
+                    }
+                });
+            }
+        }
+        else // If from Swipe to Refresh
+        {
+
+            swipeLayout.setRefreshing(true);
+            //Fetch new data
+            mService.getSources().enqueue(new Callback<WebSite>() {
+                @Override
+                public void onResponse(Call<WebSite> call, Response<WebSite> response) {
+                    adapter  = new ListSourceAdapter(getBaseContext(),response.body());
+                    adapter.notifyDataSetChanged();
+                    listWebsite.setAdapter(adapter);
+
+                    //Save to cache
+                    Paper.book().write("cache",new Gson().toJson(response.body()));
+
+                    //Dismiss refresh progressring
+                    swipeLayout.setRefreshing(false);
+                }
+
+                @Override
+                public void onFailure(Call<WebSite> call, Throwable t) {
+
+                }
+            });
+
+        }
 
 
         //  https://newsapi.org/v2/sources?language=en&apiKey=e4befc80710444afa7f93f67a5790d57
---
+++ app/src/main/java/com/elhady/newsapp/adapter/ListSourceAdapter.java
@@ -0,0 +1,77 @@
+package com.elhady.newsapp.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.elhady.newsapp.R;
+import com.elhady.newsapp.Interface.ItemClickListener;
+import com.elhady.newsapp.models.WebSite;
+
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
+class ListSourceViewHolder extends RecyclerView.ViewHolder
+        implements  View.OnClickListener {
+
+    ItemClickListener itemClickListener;
+
+    TextView source_title;
+    CircleImageView source_image;
+
+    public ListSourceViewHolder(View itemView) {
+        super(itemView);
+
+        source_image = (CircleImageView)itemView.findViewById(R.id.source_image);
+        source_title = (TextView)itemView.findViewById(R.id.source_name);
+
+        //itemView.setOnClickListener(this);
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    @Override
+    public void onClick(View view) {
+        itemClickListener.onClick(view,getAdapterPosition(),false);
+    }
+}
+
+public class ListSourceAdapter extends RecyclerView.Adapter<ListSourceViewHolder> {
+    private Context context;
+    private WebSite webSite;
+
+   // private IconBetterIdeaService mService;
+
+    public ListSourceAdapter(Context context, WebSite webSite) {
+        this.context = context;
+        this.webSite = webSite;
+
+     //   mService = Common.getIconService();
+    }
+
+
+    @Override
+    public ListSourceViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View itemView = inflater.inflate(R.layout.source_layout, parent, false);
+        return new ListSourceViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final ListSourceViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return webSite.getSources().size();
+    }
+}
